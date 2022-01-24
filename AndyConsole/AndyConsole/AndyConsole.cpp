b9e73f7c16dc4a5066c069dc00bd7fc7722dafd0
andy <reachouttoandylee@gmail.com>
2022-01-23 21:09:48 -0500

    Introduction to CPP

--- AndyConsole/AndyConsole/AndyConsole.cpp
+++ AndyConsole/AndyConsole/AndyConsole.cpp
@@ -2,19 +2,70 @@
 //
 
 #include <iostream>
+int sum(int x, int y)
+{
+    return x + y;
+}
+
+void TrySum()
+{
+    std::cout << "Hello World\n"; //cout is computer output
+    int n, m;
+
+    std::cout << "Input number:";
+    std::cin >> n; //cin is what gets user input
+    std::cin >> m;
+
+    int result = sum(n, m);
+
+    std::cout << "result is: " << result << std::endl;
+
+}
+
+
+ 
+
+void TryWhileLoop() 
+{
+    int result = 0; //initial value
+    int i = 1; //initial value
+
+    while (i <= 10) //inside brackets is condition
+    {
+        result += i; //actual function these two lines down there
+        i++; //post execution (what needs to be done to ensure loop is functional 
+    }
+    std::cout << "Sum of 1-10 is: " << result << std::endl;
+}
+
+void TryForLoop()
+{
+    int result = 0;
+    for (int i = 1; i <= 100; i++) //divided into three sections separated with ";" first box is initial value, second is condition, third is post exeution 
+    {
+        result += i;
+    }
+    std::cout << "Sum of 100 is: " << result << std::endl;
+}
+
+void TryIf()
+{
+    std::cout << "Please input a number";
+    int n; //creation of variable
+    std::cin >> n;
+    if (n % 2 == 0) //% is the remainder
+        std::cout << "Your number is even" << std::endl; //endl makes sure that the line ends
+    else
+        std::cout << "Your number is odd" << std::endl; //if else statements very similar to python
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //TrySum();
+    //TryWhileLoop();
+    //TryForLoop();
+    //TryIf();
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    
