099f54ecb89cb0e9e0ce53d205d1eb14e244a2a6
andy <reachouttoandylee@gmail.com>
2022-01-27 22:42:54 -0500

    chapter one end

--- AndyConsole/AndyConsole/AndyConsole.cpp
+++ AndyConsole/AndyConsole/AndyConsole.cpp
@@ -1,7 +1,5 @@
-// AndyConsole.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "sales_Item.h"
 int sum(int x, int y)
 {
     return x + y;
@@ -59,6 +57,77 @@ void TryIf()
         std::cout << "Your number is odd" << std::endl; //if else statements very similar to python
 }
 
+void HWTwoNum()
+{
+    int m;
+    int n;
+    int c = 0; //set up a variable that counts
+    std::cout << "Input a number";
+    std::cin >> m;
+    std::cout << "Input another number";
+    std::cin >> n;
+
+    if (m > n) //swaps the numbers no matter what order
+    {
+        int tmp = m;
+        m = n;
+        n = tmp;
+    }
+
+    while (m <= n)
+    {
+        c++; //every loop count once
+        std::cout << m << " ";
+        m++;
+        if (c % 10 == 0) //every 10 outputs make a new line
+            std::cout << std::endl;
+    }
+}
+
+void TrySalesItem()
+{
+    std::cout << "Input book1, bookno, sold, price";
+    Sales_item book1;
+    std::cin >> book1;
+    
+    std::cout << "Input book2, bookno, sold, price";
+    Sales_item book2;
+    std::cin >> book2;
+
+    if (book1.isbn() != book2.isbn())
+    {
+        std::cout << "book No. are different!" << std::endl;
+    }
+    
+    Sales_item book3 = book1 + book2;
+    std::cout << book3 << std::endl;
+}
+
+void TryCountBooks()
+{
+    Sales_item total, trans;
+
+    if (std::cin >> total)
+    {
+        while (std::cin >> trans)
+        {
+            if (total.isbn() == trans.isbn())
+                total = total + trans;
+            else
+            {
+                std::cout << total << std::endl;
+                total = trans;
+            }
+        }
+    }
+    else
+    {
+        std::cout << "Bye!" << std::endl;
+    }
+}
+
+
+
 
 int main()
 {
@@ -66,6 +135,9 @@ int main()
     //TryWhileLoop();
     //TryForLoop();
     //TryIf();
+    //HWTwoNum();
+    //TrySalesItem();
+    TryCountBooks();
 }
 
     
